ef27e8ef3f91bda65198f1aa66545695bea6f64d
Hugo Pimentel <hugobezerrapimentel@gmail.com>
2016-10-09 17:13:06 -0300

    Projeto P1 - Versão 1.2

--- app/src/main/java/android/ucam/aula/projeto/projetop1/PrincipalActivity.java
+++ app/src/main/java/android/ucam/aula/projeto/projetop1/PrincipalActivity.java
@@ -1,10 +1,14 @@
 package android.ucam.aula.projeto.projetop1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -12,6 +16,7 @@ public class PrincipalActivity extends AppCompatActivity {
     private EditText senha;
     private Button acessar;
     private Button limpar;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,25 @@ public class PrincipalActivity extends AppCompatActivity {
         senha = (EditText) findViewById(R.id.editTextSenha);
         acessar = (Button) findViewById(R.id.buttonAcessar);
         limpar = (Button) findViewById(R.id.buttonLimpar);
+        resultado = (TextView) findViewById(R.id.textViewResultado);
+
+        acessar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(email.getText().toString().equalsIgnoreCase("admin@admin.com") && senha.getText().toString().equalsIgnoreCase("12345"))
+                {
+                    Intent trocadorTela = new Intent(PrincipalActivity.this, CadastroActivity.class);
+                    startActivity(trocadorTela);
+                }else{
+                    resultado.setText("Usuário ou senha inválidos.");
+                }
+            }
+        });
 
         limpar.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 email.setText("");
                 senha.setText("");
+                resultado.setText("");
             }
         });
     }
