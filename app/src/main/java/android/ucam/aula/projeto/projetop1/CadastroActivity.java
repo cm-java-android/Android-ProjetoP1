2e72d0a31d701d081ef3a900e2507b8ae6b9ab15
Hugo Pimentel <hugobezerrapimentel@gmail.com>
2016-10-09 21:06:40 -0300

    Projeto P1 - Versão 1.3

--- app/src/main/java/android/ucam/aula/projeto/projetop1/CadastroActivity.java
+++ app/src/main/java/android/ucam/aula/projeto/projetop1/CadastroActivity.java
@@ -2,12 +2,39 @@ package android.ucam.aula.projeto.projetop1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText matricula;
+    private EditText email;
+    private EditText contaGit;
+    private Button cadastrar;
+    private Button limpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        nome = (EditText) findViewById(R.id.editTextCadNome);
+        matricula = (EditText) findViewById(R.id.editTextCadMatricula);
+        email = (EditText) findViewById(R.id.editTextCadEmail);
+        contaGit = (EditText) findViewById(R.id.editTextCadConta);
+        cadastrar = (Button) findViewById(R.id.buttonCadCadastrar);
+        limpar = (Button) findViewById(R.id.buttonCadLimpar);
+
+        limpar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                nome.setText("");
+                matricula.setText("");
+                email.setText("");
+                contaGit.setText("");
+            }
+        });
     }
 }
+
